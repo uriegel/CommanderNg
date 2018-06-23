3ffce32586a30567b2656ce3fac4b2714fd9e03c
Uwe Riegel <uriegel@hotmail.de>
2018-06-23 17:41:43 +0200

    Began with CommanderView

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { NgModule } from '@angular/core'
 import { AppComponent } from './app.component'
 import { TestAddonComponent } from './test/test-addon/test-addon.component'
 import { ScrollbarComponent as TestScrollbarComponent } from './test/scrollbar/scrollbar.component'
-import { ScrollbarComponent } from './scrollbar/scrollbar.component';
+import { ScrollbarComponent } from './scrollbar/scrollbar.component'
 import { ColumnsComponent as TestColumnsComponent } from './test/columns/columns.component'
-import { ColumnsComponent } from './columns/columns.component';
-import { IconViewComponent } from './test/icon-view/icon-view.component';
-import { TableViewComponent as TestTableViewComponent } from './test/table-view/table-view.component';
-import { TableViewComponent } from './table-view/table-view.component';
-import { FileItemComponent } from './datatemplates/file-item/file-item.component';
+import { ColumnsComponent } from './columns/columns.component'
+import { IconViewComponent } from './test/icon-view/icon-view.component'
+import { TableViewComponent as TestTableViewComponent } from './test/table-view/table-view.component'
+import { TableViewComponent } from './table-view/table-view.component'
+import { FileItemComponent } from './datatemplates/file-item/file-item.component'
 import { DirectoryItemComponent } from './datatemplates/directory-item/directory-item.component'
+import { CommanderViewComponent } from './commander-view/commander-view.component'
+import { CommanderViewComponent as TestCommanderViewComponent } from './test/commander-view/commander-view.component'
 
 @NgModule({
     declarations: [
@@ -25,7 +27,9 @@ import { DirectoryItemComponent } from './datatemplates/directory-item/directory
         TestTableViewComponent,
         TableViewComponent,
         FileItemComponent,
-        DirectoryItemComponent
+        DirectoryItemComponent,
+        CommanderViewComponent,
+        TestCommanderViewComponent
     ],
     imports: [
         BrowserModule
---
+++ src/app/test/commander-view/commander-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-commander-view',
+  templateUrl: './commander-view.component.html',
+  styleUrls: ['./commander-view.component.css']
+})
+export class CommanderViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
