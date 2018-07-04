f63a1f6b884b729602b0adef23324deb6970d05b
uriegel <uriegel@hotmail.de>
2018-07-04 14:28:46 +0200

    Commander with splitters

--- src/app/commander/commander.component.ts
+++ src/app/commander/commander.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-commander',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommanderComponent implements OnInit {
 
-  constructor() { }
+    isLastVisible = true
 
-  ngOnInit() {
-  }
+    constructor() { }
 
+    ngOnInit() { }
+
+    private onClick() {
+        this.isLastVisible = !this.isLastVisible
+    }
 }
