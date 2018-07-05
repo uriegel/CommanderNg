463ff185d2a5dbadf016bd455624b0b4229bda29
Uwe Riegel <uriegel@hotmail.de>
2018-07-05 08:01:40 +0200

    focus control in commander

--- src/app/commander/commander.component.ts
+++ src/app/commander/commander.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter, ViewChild, OnInit, NgZone } from '@angular/core'
+import { Component, Output, EventEmitter, ViewChild, OnInit, NgZone, HostListener } from '@angular/core'
 import { CommanderViewComponent } from '../commander-view/commander-view.component';
 const { ipcRenderer } = (<any>window).require('electron')
 
@@ -21,6 +21,24 @@ export class CommanderComponent implements OnInit {
 
     ngOnInit() {
         ipcRenderer.on("viewer", (_: any, on: boolean) => this.zone.run(() => this.isViewVisible = on))
+    }
+
+    @HostListener('keydown', ['$event']) 
+    private onKeydown(evt: KeyboardEvent) {
+        switch (evt.which) {
+            case 9: // tab
+            // TODO: in CommanderView: tab: to TableView, ShiftTAb: to input
+            // TODO: aks, who has the focus, then shift focus
+                let focusElement = document.activeElement.closest("#leftView")
+                if (focusElement)
+                    console.log("leftView")
+                focusElement = document.activeElement.closest("#rightView")
+                if (focusElement)
+                    console.log("rightView")
+                evt.stopPropagation()
+                evt.preventDefault()
+                break
+        }
     }
 
     private onRatioChanged() {
