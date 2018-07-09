6019ab96a380a300827b8ec193986461c28898c4
Uwe Riegel <uriegel@hotmail.de>
2018-07-09 19:38:39 +0200

    no test

--- src/app/select-all.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SelectAllDirective } from './select-all.directive';
-
-describe('SelectAllDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SelectAllDirective();
-    expect(directive).toBeTruthy();
-  });
-});
