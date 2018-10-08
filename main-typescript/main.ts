818a88a1cc53629ea329baee46ab67c603ea6269
uriegel <uriegel@hotmail.de>
2018-10-08 08:28:33 +0200

    Initializing server sent events

--- main-typescript/main.ts
+++ main-typescript/main.ts
@@ -8,17 +8,19 @@ var XMLHttpRequest = require('xhr2')
 app.on('ready', () => {
 
     console.log("Starting Commander")
-    const auguryPath = 'C:\\Users\\urieg\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Extensions\\elgalmkoelokbchhkhacckoklkejnhcd\\1.19.1_0'
-    const prc = spawn("dotnet", [ "C:\\Users\\urieg\\source\\repos\\Commander\\Commander\\bin\\Debug\\netcoreapp2.1\\Commander.dll" ])
+    //const auguryPath = 'C:\\Users\\urieg\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Extensions\\elgalmkoelokbchhkhacckoklkejnhcd\\1.19.1_0'
+    //const prc = spawn("dotnet", [ "C:\\Users\\urieg\\source\\repos\\Commander\\Commander\\bin\\Debug\\netcoreapp2.1\\Commander.dll" ])
+
+    const prc = spawn("dotnet", [ "..\\Commander\\bin\\Debug\\netcoreapp2.1\\Commander.dll" ])
     prc.stdout.on('data', data => {
         var str = data.toString()
         var lines = str.split(/(\r?\n)/g);
         console.log(lines.join(""));
     })
-    
+
     prc.on('close', code => console.log('process exit code', code))
 
-    BrowserWindow.addDevToolsExtension(auguryPath)
+    //BrowserWindow.addDevToolsExtension(auguryPath)
 
     const bounds = JSON.parse(settings.get("window-bounds", 
         JSON.stringify({ 
