65c1b439dd870dd283cc5dba8cd4aae968807b23
uriegel <uriegel@hotmail.de>
2018-10-12 19:47:41 +0200

    Themes management started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'
 import { ConnectionService } from './services/connection.service';
+import { ThemesService } from './services/themes.service';
 
 @Component({
     selector: 'app-root',
@@ -7,6 +8,7 @@ import { ConnectionService } from './services/connection.service';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    constructor(private connection: ConnectionService) { 
+    constructor(private connection: ConnectionService, themes: ThemesService) { 
+        themes.setTheme("dark")
     }
 }
---
+++ src/app/services/themes.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core'
+
+function removeThemes() {
+    let styleSheet = document.getElementById("dark")
+    if (styleSheet)
+        styleSheet.remove()
+    styleSheet = document.getElementById("blue")
+    if (styleSheet)
+        styleSheet.remove()
+    styleSheet = document.getElementById("lightblue")
+    if (styleSheet)
+        styleSheet.remove()     
+    styleSheet = document.getElementById("ubuntu")
+        if (styleSheet)
+            styleSheet.remove()    
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ThemesService {
+
+    constructor() { }
+
+    setTheme(theme: string) {
+        removeThemes()
+        const head = document.getElementsByTagName('head')[0]
+        let link = document.createElement('link')
+        link.rel = 'stylesheet'
+        link.id = 'theme'
+        link.type = 'text/css'
+        link.href = `assets/themes/${theme}.css`
+        link.media = 'all'
+        head.appendChild(link)    
+    
+        setTimeout(() => this.setTheme("blue"), 6000)    
+    } 
+}
