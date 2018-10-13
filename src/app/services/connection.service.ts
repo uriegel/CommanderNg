df2291291e29cee3e759af5be12fe739d94ab61d
uriegel <uriegel@hotmail.de>
2018-10-13 08:51:41 +0200

    ---

--- src/app/services/connection.service.ts
+++ src/app/services/connection.service.ts
@@ -29,7 +29,6 @@ export class ConnectionService {
             this.commanderSubject.next(evt.data as string)
         })
         
-        this.source.onmessage = msg => console.log("msg", msg)
         this.source.addEventListener("leftView", (evt: MessageEvent) => console.log("onEreignis", evt.data))
         this.source.addEventListener("rightView", (evt: MessageEvent) => console.log("onEreignis", evt.data))
     }
