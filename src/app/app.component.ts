0956010343f170e07e7ac955c2703489cd424e18
uriegel <uriegel@hotmail.de>
2018-10-22 20:57:09 +0200

    Themes adapted to angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,27 @@
 import { Component } from '@angular/core'
 import { ConnectionService } from './services/connection.service'
-import { ThemesService } from './services/themes.service'
 import { CommanderEvent } from './model/model'
 
 @Component({
     selector: 'app-root',
+    host: {
+		"[class.blue-theme]": "theme == 'blue'",
+        "[class.light-blue-theme]": "theme == 'lightblue'",
+        "[class.dark-theme]": "theme == 'dark'",
+        "[class.ubuntu-theme]": "theme == 'ubuntu'"
+        
+	},
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    constructor(private connection: ConnectionService, themes: ThemesService) { 
+    public theme: string;
+
+    constructor(private connection: ConnectionService) { 
         this.connection.commanderEvents.subscribe(evt => {
             const commanderEvent: CommanderEvent = JSON.parse(evt)
             if (commanderEvent.theme)
-                themes.setTheme(commanderEvent.theme)
+                this.theme = commanderEvent.theme
         })  
     }
 }
