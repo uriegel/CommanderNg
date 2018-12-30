772f16662a485834881712a6c86bfe617f758a65
uriegel <uriegel@hotmail.de>
2018-12-30 23:56:36 +0100

    Commander now ready

--- src/app/commander/commander.component.ts
+++ src/app/commander/commander.component.ts
@@ -1,8 +1,7 @@
 import { Component, ViewChild, OnInit, NgZone, HostListener, AfterViewInit, Input } from '@angular/core'
 import { CommanderViewComponent } from '../commander-view/commander-view.component'
 import { DialogComponent } from '../dialog/dialog.component'
-import { ConnectionService } from '../services/connection.service'
-import { ElectronService } from '../services/electron.service'
+import { IProcessor } from '../interfaces/commander-view';
 
 @Component({
     selector: 'app-commander',
@@ -21,16 +20,12 @@ export class CommanderComponent implements OnInit, AfterViewInit {
 
     isViewVisible = false
 
-    constructor(public connection: ConnectionService, public electron: ElectronService, private zone: NgZone) {}
+    commanderViewLeft = CommanderLeft
+    commanderViewRight = CommanderRight
 
-    ngOnInit() {
-        this.electron.onCreateFolder.subscribe(() => {
-            this.focusedView.createFolder(this.dialog)
-        })
-        // ipcRenderer.on("viewer", (_: any, on: boolean) => this.zone.run(() => this.isViewVisible = on))
-        // ipcRenderer.on("refresh", (_: any) => this.zone.run(() => this.focusedView.refresh()))
-        // ipcRenderer.on("delete", (_: any) => this.zone.run(() => this.focusedView.delete(this.dialog)))
-    }
+    constructor(private zone: NgZone) {}
+
+    ngOnInit() { }
 
     ngAfterViewInit() { setTimeout(() => this.leftView.focus()) }
 
@@ -60,3 +55,6 @@ export class CommanderComponent implements OnInit, AfterViewInit {
         this.rightView.onResize()
     }
 }
+
+declare var CommanderLeft : IProcessor
+declare var CommanderRight : IProcessor
